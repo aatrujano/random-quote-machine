2b0b60dece3f1d18e52cf6a89de8fa70036711f1
aatrujano <76922673+aatrujano@users.noreply.github.com>
2022-02-05 17:40:09 -0600

    finished project

--- src/Quote.js
+++ src/Quote.js
@@ -1,6 +1,10 @@
 // STYLE
 import './Quote.css';
 
+// IMAGES
+import tumblr from './images/tumblr.png';
+import twitter from './images/twitter.png';
+
 const Quote = ({ allQuotes, currentQuote, getQuote }) => {
   return (
     <div id='quote-box' className='container'>
@@ -19,8 +23,8 @@ const Quote = ({ allQuotes, currentQuote, getQuote }) => {
           )}
      </div>
       <div className='ctaContainer'>
-        <a href='twitter.com/intent/tweet' id='tweet-quote' className='button b1 twitter'>twitter</a>
-        <button className='button b1 tumblr'>tumblr</button>
+        <a href='twitter.com/intent/tweet' id='tweet-quote' className='button b1 twitter'><img src={twitter} alt="" /></a>
+        <a href='tumblr.com' className='button b1 tumblr'><img src={tumblr} alt="" /></a>
         <button id='new-quote' className='button b2 getNewQuote' onClick={getQuote}>
           New quote
         </button>
